41de9bf5c91c7c83c620eced1dc1ad30dc793c57
Raúl <raul.rodriguez.c.2012@gmail.com>
2025-03-21 02:06:59 -0300

    Portafolio más responsive

--- src/views/Portafolio/Portafolio.jsx
+++ src/views/Portafolio/Portafolio.jsx
@@ -1,25 +1,21 @@
-import portafolio from '../../data/portafolio.json'
-import './portafolio.css'
+import portafolio from "../../data/portafolio.json";
+import "./portafolio.css";
 
 export const Portafolio = () => {
   return (
     <div className="portafolio">
-
-            <ul>
-            {portafolio.map((portafolio)=>(
-                <li key={portafolio.id} className='portEle'>
-                    <img src={portafolio.img} alt={portafolio.title} />
-                    <h3 className='portTitle' >{portafolio.title}</h3>
-                    <p className='portDesc' >{portafolio.desc}</p>
-                    <a href={portafolio.link} target="_blank">Mira este proyecto aquí</a>
-                   
-                </li>
-
-
-            ) 
-        )}
-        </ul>
+      <ul>
+        {portafolio.map((portafolio) => (
+          <li key={portafolio.id} className="portEle">
+            <img src={portafolio.img} alt={portafolio.title} />
+            <h3 className="portTitle">{portafolio.title}</h3>
+            <p className="portDesc">{portafolio.desc}</p>
+            <a href={portafolio.link} target="_blank">
+              <strong> Mira este proyecto aquí</strong>
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
-
+  );
+};
