1fd2eba1e39155e7862af86e58246149d6e3ce4c
Raúl <raul.rodriguez.c.2012@gmail.com>
2025-03-21 14:47:21 -0300

    actualización de texto de descripción

--- src/views/Home/Descripcion/TextoDescripcion/TextoDescripcion.jsx
+++ src/views/Home/Descripcion/TextoDescripcion/TextoDescripcion.jsx
@@ -1,27 +1,25 @@
-import './textoDescripcion.css'
+import "./textoDescripcion.css";
 
 const TextoDescripción = () => {
   return (
     <>
-            <p className="mx-4 my-2 textdesc fs-5 desc1">
-          Estudiante de desarrollo Fullstack en Desafío Latam y técnico Analista
-          Programador en CFT Cenco. Previamente, profesor de inglés con un
-          grado de magister en didáctica del idioma y 8 años de experiencia en
-          enseñanza en distintos contextos. Cuento con un alto dominio
-          certificado del idioma, buenas habilidades de comunicación oral y
-          escrita tanto en inglés como en español, además de la capacidad de
-          explicar conceptos complejos de forma sencilla. <br />
-          <br />
-          Profesional apasionado, crítico y en evolución constante. Experiencia
-          usando HTML5, CSS3, y Javascript Vanilla para el desarrollo de páginas
-          y aplicaciones web frontend mediante la manipulación del DOM y el
-          desarrollo de diseños responsivos usando propiedades de CSS como Media
-          Queries, Flexbox, CSS Grid, y frameworks como Bootstrap . Apunto a
-          desenvolverme con libertad en la industria tecnológica y realizar
-          contribuciones significativas en todo lugar donde me encuentre.
-        </p>
+      <p className="mx-4 my-2 textdesc fs-5 desc1">
+        Técnico Analista Programador y estudiante de Desarrollo Fullstack
+        JavaScript en Desafío Latam y . Previamente, profesor de inglés con un
+        grado de magister en didáctica del idioma y 8 años de experiencia en
+        enseñanza en distintos contextos. Cuento con un alto dominio certificado
+        del idioma, buenas habilidades de comunicación oral y escrita tanto en
+        inglés como en español, además de la capacidad de explicar conceptos
+        complejos de forma sencilla. <br />
+        <br />
+        Profesional apasionado, crítico y en evolución constante. Experiencia
+        usando HTML5, CSS3, Javascript Vanilla,y React para el desarrollo de sitios y
+        aplicaciones web frontend modernas y con  diseño responsivo. Apunto a
+        desenvolverme con libertad en la industria tecnológica y realizar
+        contribuciones significativas en todo lugar donde me encuentre.
+      </p>
     </>
-  )
-}
+  );
+};
 
-export default TextoDescripción
+export default TextoDescripción;
