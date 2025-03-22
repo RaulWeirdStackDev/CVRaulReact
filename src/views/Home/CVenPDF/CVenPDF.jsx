b7c51b03a8c1de273d3527f7780d9a6b0cb738fc
Raúl <raul.rodriguez.c.2012@gmail.com>
2025-03-22 01:48:03 -0300

    Trabajando en responsividad footer

--- src/views/Home/CVenPDF/CVenPDF.jsx
+++ src/views/Home/CVenPDF/CVenPDF.jsx
@@ -4,8 +4,7 @@ export const CVenPDF = () => {
   return (
     <div className="cvDescargable">
     <a href="/CVRaulR.pdf" target="_blank"
-      >⬇ Descarga aquí mi curriculum en formato PDF ⬇</a
-    >
+      >⬇ Descarga aquí mi curriculum en formato PDF ⬇</a>
   </div>
   )
 }
