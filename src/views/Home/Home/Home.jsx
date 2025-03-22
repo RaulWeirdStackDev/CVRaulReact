85681ca5e48f4144517c541946e20c7879e761a6
Raúl <raul.rodriguez.c.2012@gmail.com>
2025-03-22 02:26:30 -0300

    mejorar responsividad en experiencia

--- src/views/Home/Home/Home.jsx
+++ src/views/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import {Perfil, Descripcion, CVenPDF} from '../index' 
 
+
 export const Home = () => {
   return (
     <>
     <main className="main">
     <Perfil/>
     <Descripcion/>
+
     <CVenPDF/>
     </main>
     </>
