d0dd218d0329a725ed8358a2ccbba2dbc0b1f4fb
Raúl <raul.rodriguez.c.2012@gmail.com>
2025-03-22 21:47:37 -0300

    Actualización url de LinkedIn

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,22 +10,22 @@ export const Footer = () => {
       <div className="contenedor2 mb-3">
         <div>
           <a href="https://wa.me/56972033734" target="_blank"
-            ><i className="fa-brands fa-whatsapp"></i
-          ></a>
+            ><i className="fa-brands fa-whatsapp"></i> 
+            </a>
         </div>
 
         <div>
           <a
-            href="https://www.linkedin.com/in/ra%C3%BAl-rodr%C3%ADguez-clavero-377368132/"
+            href="https://www.linkedin.com/in/raulrc86/"
             target="_blank"
-            ><i className="fa-brands fa-linkedin"></i
-          ></a>
+            ><i className="fa-brands fa-linkedin"></i>
+            </a>
         </div>
 
         <div>
           <a href="https://github.com/RaulRC86" target="_blank"
-            ><i className="fa-brands fa-github"></i
-          ></a>
+            ><i className="fa-brands fa-github"></i>
+            </a>
         </div>
       </div>
     </footer>
